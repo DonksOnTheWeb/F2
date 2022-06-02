5424233164c99fd08f8b553137f2ea364180a0aa
DonksonTheWeb <david.allcock@gmail.com>
2022-06-02 10:37:28 +0200

    v1 database handler

---
+++ app/_maria.py
@@ -0,0 +1,32 @@
+import mariadb
+import json
+
+
+def returnConnection():
+    try:
+        conn = mariadb.connect(
+            user="root",
+            password="!L3ngthBe4tYouCannot!!",
+            host="192.168.1.4",
+            port=3306,
+            database="scdb"
+        )
+    except mariadb.Error:
+        conn = None
+
+    return conn
+
+
+def getData(params):
+    j = params.get('j')
+    my_Conn = returnConnection()
+    cur = my_Conn.cursor()
+
+    statement = "SELECT * from scdata where asat = (select max(asat) from scdata where j = %s)"
+    jurisdiction = (j,)
+
+    cur.execute(statement, jurisdiction)
+
+    result = cur.fetchall()
+
+    return json.dumps(result, default=str)
