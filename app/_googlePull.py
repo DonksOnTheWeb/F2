3abcabc1c876727dac0fd63150002902e516ef39
DonksonTheWeb <david.allcock@gmail.com>
2022-07-15 15:50:14 +0200

    v1 database handler

--- app/_googlePull.py
+++ app/_googlePull.py
@@ -77,7 +77,28 @@ def writeTo(ctry, jsonData, inOff):
     return retVal
 
 
-def readFrom(sheetname, cols=None):
+def readFrom(sheetname):
+    SERVICE_ACCOUNT_FILE = 'keys.json'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    # The ID and range of a sample spreadsheet.
+    spreadsheet_id = '1Uykg22mWkNwj2v-jgYJN0WbtDAKQx9ZkF4aoRXxb6VU'
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    cols = "!B:D"
+
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=spreadsheet_id, range=sheetname + cols).execute()
+    values = result.get('values', [])
+
+    return values
+
+
+def readFromOld(sheetname, cols=None):
     SERVICE_ACCOUNT_FILE = 'keys.json'
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
@@ -100,11 +121,44 @@ def readFrom(sheetname, cols=None):
     return values
 
 
-def checkDaily(ctry, lastEntry):
+def checkDaily(lastEntry):
     retVal = {}
     date_format = "%Y-%m-%d"
     new_entries = []
-    #Do a try on the readFrom line
+    gData = readFrom('Orders_Daily_FR_UK_ES.csv')
+    gData = gData[1:]
+    haveLoaded = False
+    finalStr = "No new entries found"
+    latestFileDate = datetime.datetime.strptime("2000-01-01", date_format).date()
+    for entry in gData:
+        if len(entry[0]) > 1:
+            dte = datetime.datetime.strptime(entry[0], date_format).date()
+            delta = dte - latestFileDate
+            if delta.days >= 0:
+                latestFileDate = dte
+
+            delta = dte - lastEntry
+            if delta.days >= -5:
+                MFC = entry[1]
+                record = (entry[0], MFC, int(entry[2].replace(',', '')))
+                new_entries.append(record)
+                haveLoaded = True
+
+    loaded = loadActuals(new_entries)
+    if haveLoaded:
+        finalStr = "Loaded " + str(loaded["Data"]) + " records.  Includes attempted re-load of last 5 days"
+
+    logger('I', finalStr)
+    logger('I', "Latest file date found for is " + datetime.datetime.strftime(latestFileDate, date_format))
+    retVal["Result"] = 1
+    retVal["Data"] = loaded
+    return retVal
+
+
+def checkDailyOLD(ctry, lastEntry):
+    retVal = {}
+    date_format = "%Y-%m-%d"
+    new_entries = []
     gData = readFrom(ctry)
     gData = gData[1:]
     haveLoaded = False
@@ -135,6 +189,29 @@ def checkDaily(ctry, lastEntry):
 
 
 def gSyncActuals(countries):
+    date_format = "%Y-%m-%d"
+    result = latestActualDate()
+    final = "2000-01-01"
+    if result["Result"] == 1:
+        data = json.loads(result["Data"])[0]
+        if data["latest"] is not None:
+            final = data["latest"]
+
+        logger('I', "Last DB entry is " + final)
+        lastEntry = datetime.datetime.strptime(final, date_format).date()
+
+        localReturn = checkDaily(lastEntry)
+        if localReturn["Result"] == 0:
+            logger('W', '')
+            logger('W', str(localReturn["Data"]))
+            logger('W', '')
+    else:
+        logger('W', '')
+        logger('W', str(result["Data"]))
+        logger('W', '')
+
+
+def gSyncActualsOld(countries):
     date_format = "%Y-%m-%d"
     result = latestActualDate()
     final = "2000-01-01"
